12ae80eea1066bfe06a31d625d2a878756f64b8d
Tomas Vazquez <vazquezt2018@gmail.com>
2024-06-29 17:35:47 -0300

    feat(config): modify messages

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
+from termcolor import colored
+import typer
+
+URL = "https://dolarhoy.com/"
+
 
 class DolarScrapper:
-    def __init__(self,url) -> None:
+    def __init__(self, url) -> None:
         self.url = url
 
     def scrape_dolar_values(self) -> int:
         page = requests.get(self.url)
-        soup = BeautifulSoup(page.content, 'html.parser')
+        soup = BeautifulSoup(page.content, "html.parser")
         dolar_values = soup.find_all("div", class_="val")
         compra = int(dolar_values[0].get_text()[1:])
         venta = int(dolar_values[1].get_text()[1:])
         return compra, venta
 
-    def find_middle_number(self, number1:int, number2: int) -> int:
-        promedio = (number1 + number2) // 2
-        print(f'El promedio de compra ${number1} y venta ${number2} es: ${promedio}')
-        return promedio
+    def print_dolar_message(self, number1: int, number2: int) -> str:
+        return f"""
+            Gracias por utilizar este scrapper.
+            Los valores actuales del dolar blue son:
+            Compra: ${colored(number1, "green", "on_black")}
+            Venta: ${colored(number2, "red", "on_black")}
 
-URL = "https://dolarhoy.com/"
-scraper = DolarScrapper(URL)
+            El {colored("promedio", "blue", "on_black")} de estos valores es: {colored(f"${(number1 + number2) / 2}", "blue", "on_black")}
+        """
 
-compra,venta = scraper.scrape_dolar_values()
 
-promedio = scraper.find_middle_number(compra, venta)
+def welcome_message() -> str:
+    return f"""
+        {colored("Bienvenido!", "yellow", "on_black", attrs=["bold"])}
+        Este programa te permite obtener los valores actuales de varias monedas en Argentina
+    """
 
+
+def main() -> None:
+    print(welcome_message())
+    scraper = DolarScrapper(URL)
+    compra, venta = scraper.scrape_dolar_values()
+    print(scraper.print_dolar_message(compra, venta))
+
+
+if __name__ == "__main__":
+    typer.run(main)
